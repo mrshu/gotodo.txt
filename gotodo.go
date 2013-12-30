e86ff36120c68349546df7b5806b97aca4f85b53
mr.Shu <mr@shu.io>
2013-12-31 00:08:59 +0100

    let's rather use docopt

--- gotodo.go
+++ gotodo.go
@@ -4,11 +4,29 @@ import  (
         "fmt"
         "flag"
         "../go-todotxt"
+        "github.com/docopt/docopt.go"
 )
 
 var show_task_count = flag.Bool("n", false, "Show just task count")
 
 func main() {
+
+        usage := `Go Todo.txt
+
+Usage:
+    gotodo
+    gotodo list
+    gotodo add <task>
+    gotodo (finish|done) <id>
+
+Options:
+    -h --help     Show this screen.
+    --version     Show version.`
+
+        arguments, _ := docopt.Parse(usage, nil, true, "Go Todo.txt 0.1", false)
+
+        fmt.Println(arguments)
+
         flag.Parse()
         tasks := todotxt.LoadTaskList("todo.txt")
 
