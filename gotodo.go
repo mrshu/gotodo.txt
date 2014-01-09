feefad31367c73f20814774b4700e4a62502a4e9
mr.Shu <mr@shu.io>
2014-01-09 20:02:27 +0100

    priority has to be in default pretty print

--- gotodo.go
+++ gotodo.go
@@ -42,7 +42,7 @@ func main() {
                                  "Show finished tasks")
         cmdList.Flags().StringVarP(&sortby, "sort", "s", "prio",
                                    "Sort tasks by parameter (prio|date|len|prio-rev|date-rev|len-rev|id)")
-        cmdList.Flags().StringVarP(&prettyformat, "pretty", "", "%i %t",
+        cmdList.Flags().StringVarP(&prettyformat, "pretty", "", "%i %p %t",
                                    "Pretty print tasks")
 
         var GotodoCmd = &cobra.Command{
