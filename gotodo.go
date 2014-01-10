46b4056a24936668e0d1e1178f4b7111ffee2e60
mr.Shu <mr@shu.io>
2014-01-10 23:30:57 +0100

    cleanup

--- gotodo.go
+++ gotodo.go
@@ -55,51 +55,7 @@ func main() {
         }
 
 
-//              usage := `Go Todo.txt
-
-//      Usage:
-//          gotodo [--sort=prio|date|len|prio-rev|date-rev|len-rev|id]
-//          gotodo list [--sort=prio|date|len|prio-rev|date-rev|len-rev|id] [<keyword>]
-//          gotodo add <task>
-//          gotodo (finish|done) <id>
-//          gotodo --num-tasks
-//          gotodo -h | --help
-//          gotodo -v | --version
-
-//      Options:
-//          -h --help      Show this screen.
-//          -v --version   Show version.
-//          --num-tasks    Show number of tasks.
-//          --pretty=STR   Pretty print the task list.
-//          -f --finished  Show the finished tasks.`
-
-        //arguments, _ := docopt.Parse(usage, nil, true, "Go Todo.txt 0.1", false)
-
-        //fmt.Println(arguments)
-
 
         GotodoCmd.AddCommand(cmdList)
         GotodoCmd.Execute()
-
-      //if (arguments["--num-tasks"].(bool)) {
-      //        fmt.Println(tasks.Len())
-      //} else {
-
-      //        var by string
-      //        if arguments["--sort"] == nil {
-      //                by = "prio"
-      //        } else {
-      //                by = arguments["--sort"].(string)
-      //        }
-      //        tasks.Sort(by)
-
-      //        for _, task := range tasks {
-      //                if !task.Finished() {
-      //                        fmt.Println(task.Id(), task.RawText())
-      //                }
-      //        }
-      //}
-
-        //fmt.Println(tasks)
-        //fmt.Println(tasks.Count())
 }
