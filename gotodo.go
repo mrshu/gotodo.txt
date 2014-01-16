794ba210866385fa63543ecc2cb8c5e440c85e04
mr.Shu <mr@shu.io>
2014-01-17 00:35:07 +0100

    printf

--- gotodo.go
+++ gotodo.go
@@ -107,7 +107,7 @@ func main() {
 
                 taskid, err := strconv.Atoi(args[0])
                 if err != nil {
-                        fmt.Println("Do you really consider %v a number?", err)
+                        fmt.Printf("Do you really consider %v a number?\n", err)
                         return
                 }
 
